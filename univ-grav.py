13e36790827b489f12b54bb439d80fadb9146741
Gabriel H <gabriel.i.hooks@gmail.com>
2018-11-01 19:29:01 -0400

    Update univ-grav.py
    I accidentally used an old broken version. This is the working one.

--- univ-grav.py
+++ univ-grav.py
@@ -1,67 +1,87 @@
+"""
+Created by Gabriel Hooks
+2018-10-21
+"""
+
 import math
 
 class Mass:
-	m = 0.0
-	posX = 0.0
-	posY = 0.0
-	
-	def __init__(self, m, posX, posY):
-		self.m = m
-		self.posX = posX
-		self.posY = posY
-	
-	def get_m(self):
-		return self.m
-		
-	def get_posX(self):
-		return self.posX
-		
-	def get_posY(self):
-		return self.posY
+    m = 0.0
+    pos_x = 0.0
+    pos_y = 0.0
+
+    def __init__(self, m, posX, posY):
+        self.m = m
+        self.pos_x = posX
+        self.pos_y = posY
+
+    @property
+    def get_m(self):
+        return self.m
+
+    @property
+    def get_pos_x(self):
+        return self.pos_x
+
+    @property
+    def get_pos_y(self):
+        return self.pos_y
 
 
 # Request user input and instantiate a Mass
-m1 = input("Enter the mass of the first mass (kg): ")
-x1 = input("Enter the x-coordinate of the first mass: ")
-y1 = input("Enter the y-coordinate of the first mass: ")
+m = float(input("Enter the mass of the first mass (kg): "))
+x = float(input("Enter the x-coordinate of the first mass: "))
+y = float(input("Enter the y-coordinate of the first mass: "))
 
-mass1 = Mass(m1, x1, y1)
+mass1 = Mass(m, x, y)
+
+del m
+del x
+del y
 
 print("------------------------------------------")
 
-m2 = input("Enter the mass of the second mass (kg): ")
-x2 = input("Enter the x-coordinate of the second mass: ")
-y2 = input("Enter the y-coordinate of the second mass: ")
+m = float(input("Enter the mass of the second mass (kg): "))
+x = float(input("Enter the x-coordinate of the second mass: "))
+y = float(input("Enter the y-coordinate of the second mass: "))
 
-mass2 = Mass(m2, x2, y2)
+mass2 = Mass(m, x, y)
+
+del m
+del x
+del y
 
 print("------------------------------------------")
 
 # Gravitational constant (in newtons)
 G = 6.67 * 10 ** -11
 
-def calc_distance(m1, m2):
-	return math.sqrt((m1.get_posX() - m2.get_posX()) ** 2 + (m1.get_posY() - m2.get_posY()))
 
-def calc_force(m1, m2):
-	return G * ((m1.get_m() * m2.get_m()) / calc_distance(m1, m2) ** 2)
+def calc_distance(m1, m2):
+    return math.sqrt(((m1.get_pos_x - m2.get_pos_x) ** 2) + ((m1.get_pos_y - m2.get_pos_y) ** 2))
+
+
+def calc_force(m1, m2, r):
+    return G * ((m1.get_m * m2.get_m) / r ** 2)
+
 
 def calc_vf(m1, m2):
-	return math.sqrt(2 * calc_force(m1, m2) * calc_distance(m1, m2))
+    return math.sqrt(2 * calc_force(m1, m2, calc_distance(m1, m2)) * calc_distance(m1, m2))
 
-def calc_totalTime(m1, m2):
-	return get_vf(m1, m2) / calc_force(m1, m2)
+
+def calc_total_time(m1, m2):
+    return calc_vf(m1, m2) / calc_force(m1, m2, calc_distance(m1, m2))
+
 
 # Distance between mass1 and mass2
 r = calc_distance(mass1, mass2)
 
 # Gravitational force between mass1 and mass2
-fG = calc_force(mass1, mass2)
+f_g = calc_force(mass1, mass2, calc_distance(mass1, mass2))
 
-# Time until mass1 and mass2 collide (in seconds)
-totalTime = calc_totalTime(mass1, mass2)
+while r > 0:
+    print("DISTANCE: {} METERS\n   FORCE: {} NEWTONS\n".format(r, f_g))
+    r -= calc_force(mass1, mass2, r)
+    f_g = calc_force(mass1, mass2, r)
 
-print("DISTANCE: {} METERS\nFORCE: {} NEWTONS\nTIME: {}\n".format(r, fG, totalTime))
-
-# End the program
-input("Press 'Enter' to exit...")
+input("")
